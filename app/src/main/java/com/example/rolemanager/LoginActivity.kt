981cc36a51303ac38231c3cf26fce81e30583023
PC-imatge-2022-2023 <licenses@enti.cat>
2022-11-21 19:43:59 +0100

    Clase Kotlin

--- app/src/main/java/com/example/rolemanager/LoginActivity.kt
+++ app/src/main/java/com/example/rolemanager/LoginActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.rolemanager.campaigns.HomeActivity
 import com.example.rolemanager.databinding.ActivityLoginBinding
 import com.example.rolemanager.list.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,6 +15,7 @@ class LoginActivity : AppCompatActivity() {
     val CORRECT_PASSWORD = "123456"
 
     lateinit var binding: ActivityLoginBinding
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +27,11 @@ class LoginActivity : AppCompatActivity() {
             val username = binding.userInput.text.toString()
             val password = binding.passwordInput.text.toString()
 
-            if (username == CORRECT_USERNAME && password == CORRECT_PASSWORD) {
-                val intent = Intent(this, MainActivity::class.java)
+            firebaseAuth = FirebaseAuth.getInstance()
+            firebaseAuth.signInWithEmailAndPassword(username, password).addOnSuccessListener{
+                val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
-            } else {
+            }.addOnFailureListener{
                 Toast.makeText(this, "Incorrect user or password.", Toast.LENGTH_SHORT).show()
             }
         }
